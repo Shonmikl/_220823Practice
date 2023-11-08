c81f6a7c048abb5e8e37f56de75aa59eb1a8cf08
shonmikl <mikhailegorov676@gmail.com>
2023-11-08 13:48:23 +0100

    taski1

---
+++ src/main/java/org/example/_2023_11_8/Test1.java
@@ -0,0 +1,48 @@
+package org.example._2023_11_8;
+
+public class Test1 {
+
+    //sum all el of array
+    public static int sum(int[] arr) {
+        int result = 0;
+        //
+        return result;
+    }
+
+    //1 2 3 4 5 6 7 8 --> 1 + 8, 2 + 7, 3 + 6 ,,,,,
+    public static int sum1(int[] arr) {
+        int result = 0;
+        //
+        return result;
+    }
+
+    //min el -> 8 9 1 3 44 10 -96
+    public static int firstMin(int[] arr){
+        return 0;
+    }
+
+    //min2 el -> 8 9 1 3 44 10 -96
+    public static int secondMin(int[] arr){
+        return 0;
+    }
+
+
+
+
+
+
+
+    static int a = 100;
+    int b;
+    public static void main(String[] args) {
+        Test1.a = 55;
+
+        Test1 o1 = new Test1();
+        o1.b = 11;
+
+        Test1 o2 = new Test1();
+        o2.b = 22;
+
+        System.out.println(o1.b);
+    }
+}
