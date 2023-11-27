8841ce961a09d4eba4556d0bfc7e9c98a7c98a20
shonmikl <mikhailegorov676@gmail.com>
2023-11-27 13:23:20 +0100

    taski1

---
+++ src/main/java/org/example/_2023_11_22/Test2.java
@@ -0,0 +1,11 @@
+package org.example._2023_11_22;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int[] arr = {12, 36, 5, 4, 7, 8, 96, 5, 41, 23};
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/org/example/_2023_11_27/TaskEx1.java
@@ -0,0 +1,9 @@
+package org.example._2023_11_27;
+
+import java.util.List;
+
+public class TaskEx1 {
+    public static void main(String[] args) {
+
+    }
+}
