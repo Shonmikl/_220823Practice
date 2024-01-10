ce7a339d78b9586ec2f1489a396669416fd0fa43
shonmikl <mikhailegorov676@gmail.com>
2024-01-10 13:48:02 +0100

    file searcher

--- src/main/java/org/example/_2024_01_10/FileSearcher.java
+++ src/main/java/org/example/_2024_01_10/FileSearcher.java
@@ -1,6 +1,12 @@
 package org.example._2024_01_10;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,7 +17,7 @@ public class FileSearcher {
         List<File> fileList = new ArrayList<>(50);
         String extension = ".pdf";
 
-        searchFile(root, fileList, extension);
+        searchFile1(root, fileList, extension);
 
         for (File file : fileList) {
             System.out.println("**FOUND**: " + file.getAbsolutePath());
@@ -33,6 +39,30 @@ public class FileSearcher {
                     }
                 }
             }
+        }
+    }
+
+    private static void searchFile1(File rootFile, List<File> fileList, String extension) {
+        Path rootPath = rootFile.toPath();
+
+        try {
+            Files.walkFileTree(rootPath, new SimpleFileVisitor<>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                    if (file.toString().toLowerCase().endsWith(extension)) {
+                        fileList.add(file.toFile());
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) {
+
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            System.out.println("MISTAKE");
         }
     }
 }
