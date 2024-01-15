42de0c9a19f15c58b91b88f34302afded1836b98
shonmikl <mikhailegorov676@gmail.com>
2024-01-15 13:38:39 +0100

    regex

---
+++ src/main/java/org/example/_2024_01_15/Handler.java
@@ -0,0 +1,10 @@
+package org.example._2024_01_15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Handler {
+    List<Product> productList = new ArrayList<>();
+
+
+}
--- src/main/java/org/example/_2024_01_15/Product.java
+++ src/main/java/org/example/_2024_01_15/Product.java
@@ -1,5 +1,41 @@
 package org.example._2024_01_15;
 
+import lombok.*;
+
+/**
+ * "CATEGORY": "FOOD#1",
+ * "NAME": "Pasta#1",
+ * "FACTORY": "Barilla#1",
+ * "QUANTITY": 150,
+ * "ISPACKED": true,
+ * "ISADV": false,
+ * "PRICE": 1.49,
+ * "MARGIN": 15.0,
+ * "ISDEBT": false},
+ */
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+    private int id;
+    private Category category;
+    private String name;
+    private Factor factor;
+    private int quantity;
+    private boolean isPacked;
+    private boolean isAdv;
+    private float price;
+    private double margin;
+    private boolean isDebt;
+}
+
+enum Category {
+
+}
+
+enum Factor {
 
 }
